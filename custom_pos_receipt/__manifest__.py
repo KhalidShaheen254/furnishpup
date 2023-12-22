0eecde3c50187facd9405499c3f7d0884c057826
Khalid Shaheen <100281433+KhalidShaheen254@users.noreply.github.com>
2023-12-22 12:53:24 +0200

    init files

---
+++ custom_pos_receipt/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Post Partner Detail",
+    "author": "Khalid Shaheen",
+    'category': 'Sales/Point of Sale',
+    "depends": ["base", "point_of_sale"],
+    "data": [
+    ],
+    "assets": {
+        'point_of_sale._assets_pos': [
+            'custom_pos_receipt/static/src/**/*'
+        ]
+    },
+}
---
+++ custom_pos_receipt/static/src/app/screens/partner_list/partner_editor/partner_editor.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
+
+patch(PosStore.prototype, {
+    getReceiptHeaderData() {
+        return {
+            ...super.getReceiptHeaderData(...arguments),
+            partner: this.get_order().get_partner(),
+        };
+    },
+});
